08bdb524cddbb70d813faafd0dd6f86b6727257b
flow2c <flows2sea@gmail.com>
2024-11-11 23:12:23 +0900

    feat: create post screen 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
+import 'package:app_team2/screens/post_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: CreatePostScreen(),
+    );
   }
 }
