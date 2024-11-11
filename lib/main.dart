6d4a4a4e9eee9bb2b01d3672386fe55f517f55c0
[APP2]여범휘 <yeobh0828@naver.com>
2024-11-11 17:30:15 +0900

    fix: ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 
+import 'package:app_team2/screens/login_screen.dart';
 import 'package:app_team2/screens/main_screen.dart';
+import 'package:app_team2/screens/signup_screen.dart';
 import 'package:app_team2/widgets/post_card.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
 void main() async{
@@ -19,9 +22,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SNS',
-      home: MainScreen(),
+    return ProviderScope(  // Wrap your app with ProviderScope
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginScreen(),  // Your LoginScreen widget
+      ),
     );
   }
 }
