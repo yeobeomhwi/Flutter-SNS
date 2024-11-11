01f26aa412ef7598869083f1090252970c351705
[APP2]여범휘 <yeobh0828@naver.com>
2024-11-11 16:27:57 +0900

    feat: add bottomNav

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -11,21 +11,28 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final List<String> posts = List.generate(6, (index) => 'Post #$index');
+  final _index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton( onPressed: () {  }, icon: const Icon(Icons.tips_and_updates),),
-        title: Text('Feed'),
+        title: const Text('Feed',style: TextStyle(fontWeight: FontWeight.bold),),
         actions: [IconButton( onPressed: () {  }, icon: const Icon(Icons.notifications),),],
+        centerTitle: true,
       ),
       body: ListView.builder(
         itemCount: posts.length,
         itemBuilder: (BuildContext context, int index) {
-          return PostCard();
+          return const PostCard();
         },
       ),
+      bottomNavigationBar: BottomNavigationBar(currentIndex: _index,items:const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.add_box), label: 'Add Feed'),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Profile'),
+      ]),
     );
   }
 }
