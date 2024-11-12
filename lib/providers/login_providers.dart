dc4a4203f03916db14e340e919a158e6b3aa19d9
[APP2]여범휘 <yeobh0828@naver.com>
2024-11-12 14:52:57 +0900

    fix: 로그인 수정

--- lib/providers/login_providers.dart
+++ lib/providers/login_providers.dart
@@ -44,26 +44,17 @@ class LoginNotifier extends StateNotifier<LoginState> {
     // 유효성 검사
     if (email.isEmpty || password.isEmpty) {
       state = state.copyWith(errorMessage: '이메일과 비밀번호를 입력해주세요.');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(state.errorMessage)),
-      );
       return;
     }
 
     final emailRegex = RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
     if (!emailRegex.hasMatch(email)) {
       state = state.copyWith(errorMessage: '유효한 이메일 주소를 입력해주세요.');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(state.errorMessage)),
-      );
       return;
     }
 
     if (password.length < 6) {
       state = state.copyWith(errorMessage: '비밀번호는 6자 이상이어야 합니다.');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(state.errorMessage)),
-      );
       return;
     }
 
@@ -76,16 +67,11 @@ class LoginNotifier extends StateNotifier<LoginState> {
         state = state.copyWith(isLoggedIn: true, errorMessage: '');
         GoRouter.of(context).push('/Main'); // 로그인 성공 시 메인 화면으로 이동
       } else {
-        state = state.copyWith(errorMessage: '로그인 실패');
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(state.errorMessage)),
-        );
+        state = state.copyWith(errorMessage: '이메일과 비밀번호를 다시 확인해주세요.');
       }
     } catch (e) {
-      state = state.copyWith(errorMessage: 'Login error: ${e.toString()}');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(state.errorMessage)),
-      );
+      // Firebase에서 로그인 오류가 발생한 경우
+      state = state.copyWith(errorMessage: '이메일과 비밀번호를 다시 확인해주세요.');
     } finally {
       state = state.copyWith(isLoading: false);
     }
