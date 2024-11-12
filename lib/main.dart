1833edc4305827f00198da26306311f777c77949
[APP2]여범휘 <yeobh0828@naver.com>
2024-11-12 15:06:09 +0900

    fix: 충돌해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
-
-import 'package:app_team2/screens/login_screen.dart';
-import 'package:app_team2/screens/main_screen.dart';
-import 'package:app_team2/screens/signup_screen.dart';
-import 'package:app_team2/widgets/post_card.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/router.dart';
 import 'firebase_options.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,18 +12,19 @@ void main() async{
   runApp(const ProviderScope(child: MyApp()));
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(  // Wrap your app with ProviderScope
+    return ProviderScope(
       child: MaterialApp.router(
         routerConfig: CustomRouter.router,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
-        ), // Your LoginScreen widget
+        ),
       ),
     );
   }
