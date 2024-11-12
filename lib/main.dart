70e85a5209ec15c8a3d4745ed245a3ded2fa64ab
flow2c <flows2sea@gmail.com>
2024-11-12 10:38:09 +0900

    refactor: create_post_screen 컴포넌트화

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app_team2/screens/post_screen.dart';
+import 'package:app_team2/screens/create_post_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:app_team2/widgets/selected_image_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -13,72 +14,57 @@ class CreatePostScreen extends StatefulWidget {
 class _CreatePostScreenState extends State<CreatePostScreen> {
   final ImagePicker _picker = ImagePicker();
   final List<XFile> _pickedImages = [];
-  
-  // 카메라, 갤러리에서 이미지 1개 불러오기
-  // ImageSource.galley , ImageSource.camera 
+
+  // 이미지 선택 함수
   void getImage(ImageSource source) async {
     final XFile? image = await _picker.pickImage(source: source);
 
-    if (image != null) { // 선택된 이미지가 null이 아닐 때만 추가
+    if (image != null) {
       setState(() {
-        _pickedImages.add(image); // 안전하게 추가
+        _pickedImages.add(image);
       });
     }
   }
-  
-  // 이미지 여러개 불러오기
+
+  // 이미지 복수 선택 함수
   void getMultiImage() async {
     final List<XFile> images = await _picker.pickMultiImage();
 
- // 선택된 이미지가 null이 아닐 때만 추가
     setState(() {
       _pickedImages.addAll(images);
     });
-    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getImage(ImageSource.gallery);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Create a Post'), actions: [TextButton(onPressed: () {}, child: const Text('다음'),),],),
+      appBar: AppBar(
+        title: const Text('Create a Post'),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('다음'),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
           child: Column(
             children: [
-              // 상단에 선택된 이미지 표시
-  _pickedImages.isNotEmpty
-      ? SizedBox(
-          height: 400, // 이미지 표시할 높이
-          width: double.infinity,
-          child: Image.file(
-            File(_pickedImages.last.path), // 가장 최근에 선택된 이미지
-            fit: BoxFit.cover,
-          ),
-        )
-      : const SizedBox(
-          height: 400, // 아이콘을 표시할 높이
-          width: double.infinity,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.photo,
-                  size: 100, // 아이콘 크기
-                  color: Colors.grey, // 아이콘 색상
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '사진이 선택되지 않았습니다.',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ],
-            ),
-          ),
-        ),
+              // 최근에 선택된 이미지 표시
+              SelectedImagePreview(pickedImages: _pickedImages),
               const SizedBox(height: 20),
+              // 이미지 불러오기 버튼
               _imageLoadButtons(),
               const SizedBox(height: 20),
+              // 선택된 이미지 gridView
               _gridPhoto(),
             ],
           ),
@@ -86,8 +72,8 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
       ),
     );
   }
-  
-  // 화면 상단 버튼
+
+  // 이미지 불러오기 버튼
   Widget _imageLoadButtons() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -118,8 +104,8 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
       ),
     );
   }
-  
-  // 불러온 이미지 gridView
+
+  // 선택된 이미지 gridView
   Widget _gridPhoto() {
     return Expanded(
       child: _pickedImages.isNotEmpty
@@ -128,7 +114,6 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
                 crossAxisCount: 3,
               ),
               children: _pickedImages
-                  .where((element) => element != null)
                   .map((e) => _gridPhotoItem(e))
                   .toList(),
             )
