58f62ea471493131930cf69863ae73ccbe074c51
[APP2]여범휘 <yeobh0828@naver.com>
2024-11-18 19:28:30 +0900

    feat: feed화면 네트워크 상태 창 추가

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,11 @@
-import 'package:app_team2/widgets/post_card.dart';
+import 'dart:async';
 
+import 'package:app_team2/widgets/post_card.dart';
+import 'package:app_team2/widgets/top_network_bar.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../providers/post/post_provider.dart';
 
@@ -12,6 +17,63 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
+  // 연결 상태를 저장하는 리스트
+  List<ConnectivityResult> _connectionStatus = [ConnectivityResult.none];
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    // 연결 상태 변화 감지를 위한 스트림 리스너 등록
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+    initConnectivity();
+  }
+
+  @override
+  void dispose() {
+    // 스트림 리스너 해제
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  // 초기 연결 상태 확인
+  Future<void> initConnectivity() async {
+    late List<ConnectivityResult> result;
+    try {
+      result = await _connectivity.checkConnectivity(); // 연결 상태 확인
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+    if (!mounted) {
+      return Future.value(null);
+    }
+    return _updateConnectionStatus(result);
+  }
+
+  // 연결 상태가 변경될 때마다 호출되는 함수
+  Future<void> _updateConnectionStatus(List<ConnectivityResult> result) async {
+    setState(() {
+      _connectionStatus = result;
+      // 와이파이나 모바일 네트워크가 연결되었으면
+      if (result.contains(ConnectivityResult.wifi) ||
+          result.contains(ConnectivityResult.mobile)) {
+        TopNetworkBar.off(); // 상단 네트워크 바 숨김
+        print('와이파이 또는 모바일 네트워크 연결됨');
+        // 네트워크 연결 시 온라인 데이터 로드
+      } else {
+        print('인터넷 연결 없음');
+        // 네트워크 연결 없음 시 상단에 네트워크 메시지 띄움
+        TopNetworkBar.on(context);
+        // 오프라인 데이터 로드
+      }
+    });
+    // 연결 상태 출력
+    print('Connectivity changed: $_connectionStatus');
+  }
+
   @override
   Widget build(BuildContext context) {
     // postProvider에서 상태를 가져옵니다.
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -21,6 +21,7 @@ class ProfileScreen extends ConsumerStatefulWidget {
 }
 
 class _ProfileScreenState extends ConsumerState<ProfileScreen> {
+  // 연결 상태를 저장하는 리스트
   List<ConnectivityResult> _connectionStatus = [ConnectivityResult.none];
   final Connectivity _connectivity = Connectivity();
   late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
@@ -28,6 +29,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
   @override
   void initState() {
     super.initState();
+    // 연결 상태 변화 감지를 위한 스트림 리스너 등록
     _connectivitySubscription =
         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
     initConnectivity();
@@ -35,14 +37,16 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
 
   @override
   void dispose() {
+    // 스트림 리스너 해제
     _connectivitySubscription.cancel();
     super.dispose();
   }
 
+  // 초기 연결 상태 확인
   Future<void> initConnectivity() async {
     late List<ConnectivityResult> result;
     try {
-      result = await _connectivity.checkConnectivity();
+      result = await _connectivity.checkConnectivity(); // 연결 상태 확인
     } on PlatformException catch (e) {
       developer.log('Couldn\'t check connectivity status', error: e);
       return;
@@ -53,27 +57,31 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
     return _updateConnectionStatus(result);
   }
 
+  // 연결 상태가 변경될 때마다 호출되는 함수
   Future<void> _updateConnectionStatus(List<ConnectivityResult> result) async {
     var uid = FirebaseService().getCurrentUser()?.uid;
     setState(() {
       _connectionStatus = result;
+      // 와이파이나 모바일 네트워크가 연결되었으면
       if (result.contains(ConnectivityResult.wifi) ||
           result.contains(ConnectivityResult.mobile)) {
-        TopNetworkBar.off();
+        TopNetworkBar.off(); // 상단 네트워크 바 숨김
         print('와이파이 또는 모바일 네트워크 연결됨');
+        // 네트워크 연결 시 온라인 데이터 로드
         WidgetsBinding.instance.addPostFrameCallback((_) {
           ref.read(profileProvider.notifier).loadUserDataOnline(uid);
         });
       } else {
         print('인터넷 연결 없음');
-        // 상단에 토스트 메시지 띄우기
+        // 네트워크 연결 없음 시 상단에 네트워크 메시지 띄움
         TopNetworkBar.on(context);
+        // 오프라인 데이터 로드
         WidgetsBinding.instance.addPostFrameCallback((_) {
           ref.read(profileProvider.notifier).loadUserDataOffline(uid);
         });
       }
     });
-    // ignore: avoid_print
+    // 연결 상태 출력
     print('Connectivity changed: $_connectionStatus');
   }
 
@@ -81,6 +89,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
   Widget build(BuildContext context) {
     final profileState = ref.watch(profileProvider);
 
+    // 프로필 로딩 중일 때 화면
     if (profileState.isLoading) {
       return Scaffold(
         appBar: AppBar(title: const Text('Profile')),
@@ -88,6 +97,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
       );
     }
 
+    // 에러가 있을 때 화면
     if (profileState.error != null) {
       return Scaffold(
         appBar: AppBar(title: const Text('Profile')),
@@ -97,15 +107,8 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
 
     final user = profileState.user;
 
-    if (user == null) {
-      return Scaffold(
-        appBar: AppBar(title: const Text('Profile')),
-        body: const Center(child: Text('사용자 정보를 찾을 수 없습니다.')),
-      );
-    }
-
-    // Profile image refresh handling
-    final imageProvider = NetworkImage(user.photoURL);
+    // 프로필 이미지 갱신 처리
+    final imageProvider = NetworkImage(user!.photoURL);
     imageProvider.evict().then((_) {
       setState(() {});
     });
@@ -114,88 +117,88 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
       appBar: AppBar(title: const Text('Profile')),
       body: Center(
         child: profileState.isLoading
-            ? Center(child: CircularProgressIndicator()) // 로딩 스피너 표시
+            ? Center(child: CircularProgressIndicator()) // 로딩 중 스피너 표시
             : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Profile image
-            CircleAvatar(
-              backgroundColor: Colors.grey,
-              backgroundImage: FileImage(
-                File('${user.photoURL}'),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // 프로필 이미지 표시
+                  CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    backgroundImage: FileImage(
+                      File('${user.photoURL}'),
+                    ),
+                    radius: 100.w,
+                  ),
+                  SizedBox(height: 16.h),
+
+                  // 사용자 이름 표시
+                  Text(
+                    '이름: ${user.displayName ?? '이름 없음'}',
+                    style: TextStyle(fontSize: 18.sp),
+                  ),
+
+                  SizedBox(height: 16.h),
+
+                  // 사용자 이메일 표시
+                  Text(
+                    '이메일: ${user.email ?? '이메일 없음'}',
+                    style: TextStyle(fontSize: 18.sp),
+                  ),
+
+                  SizedBox(height: 16.h),
+
+                  // 사용자 UID 표시
+                  Text(
+                    'UID: ${user.uid}',
+                    style: TextStyle(fontSize: 18.sp),
+                  ),
+
+                  SizedBox(height: 16.h),
+
+                  // 구분선
+                  Padding(
+                    padding: EdgeInsets.all(16.w),
+                    child: Divider(),
+                  ),
+
+                  // 프로필 사진 변경 버튼
+                  InfinityButton(
+                    onPressed: () async {
+                      final picker = ImagePicker();
+                      final pickedFile =
+                          await picker.pickImage(source: ImageSource.gallery);
+
+                      if (pickedFile != null) {
+                        final imageFile = File(pickedFile.path);
+                        ref
+                            .read(profileProvider.notifier)
+                            .updateProfilePicture(user.uid, imageFile);
+                      }
+                    },
+                    title: '프로필 사진 변경',
+                  ),
+
+                  SizedBox(height: 5.h),
+
+                  // 로그아웃 버튼
+                  InfinityButton(
+                    onPressed: () async {
+                      try {
+                        await FirebaseService().signOut(); // 로그아웃 처리
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('로그아웃 되었습니다.')),
+                        );
+                        GoRouter.of(context).push('/Login');
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('로그아웃 실패: $e')),
+                        );
+                      }
+                    },
+                    title: '로그아웃',
+                  ),
+                ],
               ),
-              radius: 100.w,
-            ),
-            SizedBox(height: 16.h),
-
-            // Name
-            Text(
-              '이름: ${user.displayName ?? '이름 없음'}',
-              style: TextStyle(fontSize: 18.sp),
-            ),
-
-            SizedBox(height: 16.h),
-
-            // Email
-            Text(
-              '이메일: ${user.email ?? '이메일 없음'}',
-              style: TextStyle(fontSize: 18.sp),
-            ),
-
-            SizedBox(height: 16.h),
-
-            // UID
-            Text(
-              'UID: ${user.uid}',
-              style: TextStyle(fontSize: 18.sp),
-            ),
-
-            SizedBox(height: 16.h),
-
-            // Divider
-            Padding(
-              padding: EdgeInsets.all(16.w),
-              child: Divider(),
-            ),
-
-            // Change profile picture button
-            InfinityButton(
-              onPressed: () async {
-                final picker = ImagePicker();
-                final pickedFile =
-                await picker.pickImage(source: ImageSource.gallery);
-
-                if (pickedFile != null) {
-                  final imageFile = File(pickedFile.path);
-                  ref
-                      .read(profileProvider.notifier)
-                      .updateProfilePicture(user.uid, imageFile);
-                }
-              },
-              title: '프로필 사진 변경',
-            ),
-
-            SizedBox(height: 5.h),
-
-            // Logout button
-            InfinityButton(
-              onPressed: () async {
-                try {
-                  await FirebaseService().signOut();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('로그아웃 되었습니다.')),
-                  );
-                  GoRouter.of(context).push('/Login');
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('로그아웃 실패: $e')),
-                  );
-                }
-              },
-              title: '로그아웃',
-            ),
-          ],
-        ),
       ),
     );
   }
